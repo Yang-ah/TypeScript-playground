75eb893bb7f4a26af83fc8be85fafcc7adafeeba
Yang-ah <yangah0409@gmail.com>
2022-11-18 17:40:22 +0900

    added Fns

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,50 @@ type Words = {
   [key: string]: string;
 };
 
+class Word {
+  constructor(readonly term: string, readonly def: string) {}
+}
+
 class Dict {
   private words: Words;
   constructor() {
     this.words = {};
   }
+
+  /* add word */
+  add(word: Word) {
+    return (this.words[word.term] = word.def);
+  }
+
+  /* get definition of word */
+  get(word: Word) {
+    return this.words[word.term] ?? "없음";
+  }
+
+  /* delete word*/
+  delete(word: Word) {
+    delete this.words[word.term];
+  }
+
+  /* update definition of word */
+  update(word: Word, newDef: string) {
+    return (this.words[word.def] = newDef);
+  }
+
+  /* show all words of dictionary */
+  showAll(dict: Dict) {
+    return dict.words;
+  }
+
+  /* return number of words */
+  count(dict: Dict) {
+    return Object.keys(dict.words).length;
+  }
 }
+
+/* new dictionary */
+const menu = new Dict();
+
+/* new word */
+const iceAmericano = new Word("Ice Americano", "얼음을 넣은 아메리카노");
+const hotAmericano = new Word("Hot Americano", "따뜻한 아메리카노");
