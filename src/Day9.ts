413c56163321c862170f1b77c37a0f52ac683882
Yang-ah <yangah0409@gmail.com>
2022-11-23 17:49:54 +0900

    added methods

--- src/Day9.ts
+++ src/Day9.ts
@@ -4,4 +4,22 @@ interface SStorage<T> {
 
 class LocalStorage<T> {
   private storage: SStorage<T> = {};
+  set(key: string, value: T) {
+    this.storage[key] = value;
+  }
+  remove(key: string) {
+    delete this.storage[key];
+  }
+  get(key: string): T {
+    return this.storage[key];
+  }
+  clear() {
+    this.storage = {};
+  }
 }
+
+const stringStorage = new LocalStorage<string>();
+stringStorage.get("ket");
+
+const booleanStorage = new LocalStorage<boolean>();
+booleanStorage.get("xxx");
